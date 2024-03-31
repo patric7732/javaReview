4234ca5fa38f75a501d984529c90acc68acac53c
patric <wns0818@naver.com>
2024-04-01 00:41:07 +0900

    feat: S_5_14916 거스름돈

---
+++ algorithm/baekjoon/dynamic_programming/S_5_14916.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S_5_14916 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        while(true){
+            if(N%5 == 0){
+                count += N/5;
+                System.out.println(count);
+                break;
+            }else{
+                N -= 2;
+                count++;
+            }
+
+            if(N < 0){
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
