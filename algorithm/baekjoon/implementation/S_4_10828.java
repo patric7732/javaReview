c988cf79a71fd7b76eb3fed2cc8848f5dce2385b
patric <wns0818@naver.com>
2024-04-02 22:05:11 +0900

    feat: S_4_10828 스택

---
+++ algorithm/baekjoon/implementation/S_4_10828.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class S_4_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        int num  = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < num; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+
+            if (s.equals("push")) {
+                stack.push(Integer.parseInt(st.nextToken()));
+            }
+
+            if (s.equals("pop")) {
+                if (stack.isEmpty()) {
+                    sb.append(-1 + "\n");
+                } else {
+                    sb.append(stack.pop() + "\n");
+                }
+            }
+
+            if (s.equals("size")) {
+                sb.append(stack.size() + "\n");
+            }
+
+            if (s.equals("empty")) {
+                if (stack.isEmpty()) {
+                    sb.append(1 + "\n");
+                } else {
+                    sb.append(0 + "\n");
+                }
+            }
+
+            if (s.equals("top")) {
+                if (stack.isEmpty()) {
+                    sb.append(-1 + "\n");
+                } else {
+                    sb.append(stack.peek() + "\n");
+                }
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
