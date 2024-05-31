a9b7d24fcedd9df90bd77f621d868e9d2508e48b
patric <wns0818@naver.com>
2024-05-31 13:14:14 +0900

    featL java_review - springdatajpa 내용 추가

--- java_review/springdatajpa/src/test/java/com/example/springdatajpa2/CustomerRepositoryTest.java
+++ java_review/springdatajpa/src/test/java/com/example/springdatajpa2/CustomerRepositoryTest.java
@@ -1,24 +1,28 @@
 package com.example.springdatajpa2;
 
-import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import java.util.List;
 import static org.assertj.core.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class CustomerRepositoryTest {
+    private static final Logger log = LoggerFactory.getLogger(CustomerRepositoryTest.class);
     @Autowired
     CustomerRepository repository;
-    private static final Logger log = LoggerFactory.getLogger(CustomerRepository.class);
 
     @Test
-    void save() {
+    void save(){
         Customer customer = new Customer("zi", "zi@naver.com");
         repository.save(customer);
 
@@ -26,23 +30,117 @@ class CustomerRepositoryTest {
     }
 
     @Test
-    void delete() {
-        repository.deleteById(10L);
-        Assertions.assertThat(repository.findById(10L)).isNotPresent();
+    void delete(){
+
     }
 
     @Test
+    @DisplayName("고객명으로 조회")
     void findByName(){
-        List<Customer> customers = repository.findByName("홍길동");
-        assertThat(customers.size()).isEqualTo(1);
+
+//         List<Customer> customers = repository.findByName("홍길동");
+//
+//        for (Customer customer : customers) {
+//            log.info(customer.getName()+" :: " + customer.getEmail());
+//        }
+
+        //Given
+        Customer customer1 = new Customer("user1","user1@test.com");
+        Customer customer2 = new Customer("user2","user1@test.com");
+        Customer customer3 = new Customer("user3","user3@test.com");
+        repository.save(customer1);
+        repository.save(customer2);
+        repository.save(customer3);
+
+        //When
+        List<Customer> user1 = repository.findByName("user1");
+
+        //Then
+
+
+
     }
 
     @Test
-    void findByEmail(){
-        List<Customer> customers = repository.findByEmail("hong@example.com");
-        assertThat(customers.size()).isEqualTo(1);
+    void findByEmailContaining(){
+//        List<Customer> exams = repository.findByEmailContaining("exam");
+//        exams.forEach(customer -> log.info(customer.getName()+" :: " + customer.getEmail()));
+
+        //Given
+        Customer customer1 = new Customer("user1","user1@test.com");
+        Customer customer2 = new Customer("user2","user1@test.com");
+        Customer customer3 = new Customer("user3","user3@test.com");
+        repository.save(customer1);
+        repository.save(customer2);
+        repository.save(customer3);
+
+        //When
+        List<Customer> customers = repository.findByEmailContaining("test");
+
+        //Then
+        Assertions.assertThat(customers).hasSize(3);
     }
 
+    @Test
+    void findCustomerOrderCount(){
+        List<Object[]> customerOrderCount = repository.findCustomerOrderCount();
+        customerOrderCount.forEach(result ->{
+            Customer customer = (Customer) result[0];
+            Long count = (Long)result[1];
+            log.info("고객이름 : {} , 주문 수 : {}",customer.getName(),count);
+        });
+    }
 
+    @Test
+    void testFindByName() {
+    }
 
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void testFindByEmailContaining() {
+    }
+
+    @Test
+    void testFindCustomerOrderCount() {
+    }
+
+    @Test
+    void findCustomersWithLatestOrder() {
+    }
+
+    @Test
+    @SqlGroup({
+            @Sql(value = "classpath:db/test.sql",
+                    config = @SqlConfig(encoding = "utf-8", separator = ";", commentPrefix = "--"),
+                    executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    })
+    void findCustomersOlderThanAverage() {
+        List<Customer> customersOlderThanAverage = repository.findCustomersOlderThanAverage();
+
+        assertThat(customersOlderThanAverage).hasSize(4)
+                .extracting(Customer::getName)
+                .contains("최지현");
+
+    }
+
+    @BeforeAll
+    static void setUp(){
+        log.info("---------> BeforeAll");
+    }
+    @AfterAll
+    static void tearDown(){
+        log.info("---------> AfterAll");
+    }
+
+    @BeforeEach
+    void BeforeEach(){
+        log.info("---------> BeforeEach");
+    }
+    @AfterEach
+    void AfterEach(){
+        log.info("---------> AfterEach");
+    }
 }
