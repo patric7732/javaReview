f4f094a7a20975deccabaa8167cd547b26db245d
PATRIC <wns0818@naver.com>
2024-07-01 10:39:16 +0900

    feat: update securityexam

---
+++ java_review/securityexam/src/main/java/org/example/securityexam4/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package org.example.securityexam4.repository;
+
+import org.example.securityexam4.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
